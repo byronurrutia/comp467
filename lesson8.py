e889bd0b7df271e23e2bdacb5e6a7902a11d5f5c
byronurrutia <b.urrutia@me.com>
2023-04-23 22:41:25 -0700

    lesson 8 done

---
+++ lesson8.py
@@ -0,0 +1,25 @@
+import subprocess
+import shlex
+import os
+
+command = "ls -l /home/byronurrutia/projects/comp467/"
+largest_file_size = 0
+largest_file_name = ""
+process = subprocess.Popen(
+    shlex.split(command),
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    shell=True
+)
+
+for line in iter(process.stdout.readline, b""):
+    parse_line = line.decode().strip()
+    current_file_size = os.path.getsize(
+        "/home/byronurrutia/projects/comp467/" + parse_line)
+    print("file: %s; size: %s" % (parse_line, current_file_size))
+    if current_file_size >= largest_file_size:
+        largest_file_size = current_file_size
+        largest_file_name = parse_line
+
+print("largest file was %s with a size of %s bytes" %
+      (largest_file_name, largest_file_size))
